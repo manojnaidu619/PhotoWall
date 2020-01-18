14e7aa25f455a921c9b738faecbbc4eba2407f02
Manoj Naidu <manojnaidu619@gmail.com>
2020-01-18 15:55:31 +0530

    First commit

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Title from './Title'
+
+class Main extends Component{
+  render(){
+    return(
+      <div class="title">
+        <Title/>
+      </div>
+    )
+  }
+}
+
+export default Main
